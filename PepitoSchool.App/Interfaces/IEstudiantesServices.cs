20aa1510c3489b0a0ce01a9ce3a18b5836d2695d
Steffania Salgado <86125323+SalgadoSteffania@users.noreply.github.com>
2022-05-19 21:03:50 -0600

    Add files via upload

---
+++ PepitoSchool.App/Interfaces/IEstudiantesServices.cs
@@ -0,0 +1,16 @@
+﻿using PepitoSchool.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepitoSchool.App.Interfaces
+{
+    public interface IEstudiantesServices: IService<Estudiante>
+    {
+        Estudiante FindByCarnet(string carnet);
+        List<Estudiante> FindByNames(string names);
+        List<Estudiante> FindByLastnames(string lastnames);
+    }
+}
---
+++ PepitoSchool.App/Services/EstudianteServices.cs
@@ -0,0 +1,64 @@
+﻿using PepitoSchool.App.Interfaces;
+using PepitoSchool.Domain.Entities;
+using PepitoSchool.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepitoSchool.App.Services
+{
+    public class EstudianteServices : IEstudiantesServices
+    {
+        public IEstudianteRepository estudianteRepository;
+        public EstudianteServices(IEstudianteRepository estudianteRepository)
+        {
+            this.estudianteRepository = estudianteRepository;
+        }
+        public void Create(Estudiante t)
+        {
+           if(t== null)
+            {
+                throw new ArgumentNullException("El estudiante no puede ser null.");
+            }
+
+            estudianteRepository.Create(t);
+        }
+
+
+        public bool Delete(Estudiante t)
+        {
+            if (t == null)
+            {
+                throw new ArgumentNullException("El estudiante no puede ser null.");
+            }
+            return estudianteRepository.Delete(t);
+        }
+
+        public Estudiante FindByCarnet(string carnet)
+        {
+            return estudianteRepository.FindByCarnet(carnet);
+        }
+
+        public List<Estudiante> FindByLastnames(string lastnames)
+        {
+            return estudianteRepository.FindByLastnames(lastnames);
+        }
+
+        public List<Estudiante> FindByNames(string names)
+        {
+            return estudianteRepository.FindByNames(names);
+        }
+
+        public List<Estudiante> GetAll()
+        {
+            return estudianteRepository.GetAll();
+        }
+
+        public int Update(Estudiante t)
+        {
+            return estudianteRepository.Update(t);
+        }
+    }
+}
